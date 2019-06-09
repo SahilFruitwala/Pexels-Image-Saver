8c5a44ce3b7fef682a06ce3870cdb7bf2b3fe2ac
SahilFruitwala <sahilfruitwala007@gmail.com>
2019-06-09 12:37:08 +0530

    Adding Css Selector

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-# using Find() method
+# using CSS selector
 
 import os
 import requests  # for requesting to url
@@ -12,12 +12,12 @@ URL_PEXELS = "https://www.pexels.com/search/"
 req = requests.get(URL_PEXELS + find_name + "/")
 
 soup = BeautifulSoup(req.content, 'html.parser')
-image = soup.find_all('img', class_='photo-item__img')  # find picture element
+image = soup.select('img[class="photo-item__img"]')  # find picture element
 
 for i in image:
     if not os.path.exists(find_name):
         os.makedirs(find_name)  # create directory of find name if not exists
-    image_url = str(i['data-big-src'])  # get 'data-big-src'  attribute value
+    image_url = str(i['data-big-src'])  # get'data-big-src' attribute value
     image_name = str(i['alt']).replace(' ', '_').replace('-', '_')
     img_data = requests.get(image_url).content  # get image data from url
     with open(find_name + '/' + image_name + '.jpg', 'wb') as handler:
