49e1ecfff7ca925c9b09cc51e96e6f1fb0fb682b
SahilFruitwala <sahilfruitwala007@gmail.com>
2019-06-09 12:36:16 +0530

    Added CSS Selector

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ from bs4 import BeautifulSoup # to go through various elements of webpage
 
 find_name = input('What do you want to find?\n') # get input from user
 
-URL = "https://www.pexels.com/search/"
-req = requests.get(URL + find_name + "/")
+URL_PEXELS = "https://www.pexels.com/search/"
+URL_UNSPLASH = "https://unsplash.com/search/photos/"
+
+req = requests.get(URL_PEXELS + find_name + "/")
 
 soup = BeautifulSoup(req.content, 'html.parser')
 image = soup.find_all('img', class_ = 'photo-item__img') # find picture element
@@ -16,8 +18,26 @@ for i in image:
     if not os.path.exists(find_name):
         os.makedirs(find_name) # create directory of find name if not exists
     image_url = str(i['data-big-src']) # get 'data-big-src'  attribute value
-    image_name = image_url.replace('https://images.pexels.com/photos/','')
-    image_name = image_name[:image_name.find('/')]
+    image_name = str(i['alt']).replace(' ','_').replace('-','_')
     img_data = requests.get(image_url).content # get image data from url
     with open(find_name + '/' + image_name + '.jpg', 'wb') as handler:
-        handler.write(img_data) # wrting data to file
+        handler.write(img_data) # wrting data to file?
+
+req = requests.get(URL_UNSPLASH + find_name + "/")
+
+soup = BeautifulSoup(req.content, 'html.parser')
+div = soup.find_all('div', class_ = 'IEpfq')
+for i in div:
+	image = i.find_all('img', class_ = '_2zEKz') # find picture element
+
+	for i in image:
+		try:
+			if not os.path.exists(find_name):
+				os.makedirs(find_name) # create directory of find name if not exists
+			image_url = str(i['src']) # get 'data-big-src'  attribute value
+			image_name = str(i['alt']).replace(' ','_').replace('-','_')
+			img_data = requests.get(image_url).content # get image data from url
+			with open(find_name + '/' + image_name + '.jpg', 'wb') as handler:
+				handler.write(img_data) # wrting data to file
+		except Exception as e:
+			print(e)
